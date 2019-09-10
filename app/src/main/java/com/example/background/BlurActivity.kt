e020ca10b65611bf1a2bc417ec5f72e6c8fc8e7f
agis <agis.septiyanto@gmail.com>
2019-09-11 00:54:35 +0700

    initial commit

--- app/src/main/java/com/example/background/BlurActivity.kt
+++ app/src/main/java/com/example/background/BlurActivity.kt
@@ -16,6 +16,7 @@
 
 package com.example.background
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +25,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.RadioGroup
+import androidx.lifecycle.Observer
+import androidx.work.WorkInfo
 import com.bumptech.glide.Glide
 
 
@@ -52,6 +55,48 @@ class BlurActivity : AppCompatActivity() {
             Glide.with(this).load(imageUri).into(imageView)
         }
 
+        outputButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+                val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+                actionView.resolveActivity(packageManager)?.run {
+                    startActivity(actionView)
+                }
+            }
+        }
+
+        cancelButton.setOnClickListener {
+            viewModel.cancelWork()
+        }
+
+        setOnClickListener()
+
+        viewModel.outputWorkInfos.observe(this, workInfosObserver())
+    }
+
+    private fun workInfosObserver(): Observer<List<WorkInfo>> {
+        return Observer { listOfWorkInfo ->
+            if(listOfWorkInfo.isNullOrEmpty()){
+                return@Observer
+            }
+
+            val workInfo = listOfWorkInfo[0]
+            if(workInfo.state.isFinished){
+                showWorkFinished()
+
+                val ouputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+
+                if(!ouputImageUri.isNullOrEmpty()){
+                    viewModel.setOutputUri(ouputImageUri)
+                    outputButton.visibility = View.VISIBLE
+                }
+            }else{
+                showWorkInProgress()
+            }
+        }
+    }
+
+    fun setOnClickListener(){
+        goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
     }
 
     private fun bindResources() {
--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -19,12 +19,23 @@ package com.example.background
 import android.app.Application
 import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.work.*
+import com.example.background.workers.BlurWorker
+import com.example.background.workers.CleanupWorker
+import com.example.background.workers.SaveImageToFileWorker
 
 
 class BlurViewModel(application: Application) : AndroidViewModel(application) {
 
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
+    private val workManager = WorkManager.getInstance(application)
+
+    internal val outputWorkInfos: LiveData<List<WorkInfo>>
+    init {
+        outputWorkInfos = workManager.getWorkInfosByTagLiveData(TAG_OUTPUT)
+    }
 
     private fun uriOrNull(uriString: String?): Uri? {
         return if (!uriString.isNullOrEmpty()) {
@@ -44,4 +55,39 @@ class BlurViewModel(application: Application) : AndroidViewModel(application) {
     internal fun setOutputUri(outputImageUri: String?) {
         outputUri = uriOrNull(outputImageUri)
     }
+
+    internal fun applyBlur(blurLevel: Int) {
+        var continuation = workManager.beginUniqueWork(IMAGE_MANIPULATION_WORK_NAME, ExistingWorkPolicy.REPLACE, OneTimeWorkRequest.from(CleanupWorker::class.java))
+
+        for (i in 0 until blurLevel) {
+            val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+            if (i == 0) {
+                blurBuilder.setInputData(createInputDataForUri())
+            }
+
+            continuation = continuation.then(blurBuilder.build())
+        }
+
+        val constraints = Constraints.Builder().setRequiresCharging(true).build()
+
+
+        val save = OneTimeWorkRequest.Builder(SaveImageToFileWorker::class.java).setConstraints(constraints).addTag(TAG_OUTPUT).build()
+
+        continuation = continuation.then(save)
+
+        continuation.enqueue()
+    }
+
+    private fun createInputDataForUri(): Data {
+        val builder = Data.Builder()
+        imageUri?.let { builder.putString(KEY_IMAGE_URI, imageUri.toString()) }
+        return builder.build()
+    }
+
+    internal fun cancelWork(){
+        workManager.cancelUniqueWork(IMAGE_MANIPULATION_WORK_NAME)
+    }
+
+
 }
